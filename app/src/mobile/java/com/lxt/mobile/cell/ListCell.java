5ad445e438ae0cb4b2e8528d3f637a6aa60802fd
yangfu <yangfu@163.com>
2017-06-09 10:24:31 +0800

    删除其他代码

--- app/src/mobile/java/com/lxt/mobile/cell/ListCell.java
+++
@@ -1,7 +0,0 @@
-package com.lxt.mobile.cell;
-
-import android.support.v7.widget.RecyclerView;
-
-public interface ListCell {
-    public void setData(Object data, int position, RecyclerView.Adapter mAdapter);
-}
--- app/src/mobile/java/com/lxt/mobile/widget/swipeMenuListView/SwipeMenuCreator.java
+++
@@ -1,6 +0,0 @@
-package com.lxt.mobile.widget.swipeMenuListView;
-
-public interface SwipeMenuCreator {
-
-    void create(SwipeMenu menu);
-}
